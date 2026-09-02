9550ae154083ef8455ee71734d9d60825592844e
Samu Jimenez <sejh200612@gmail.com>
2026-09-02 12:18:20 -0500

    Agregar archivo ejemplo ejemplo_hw a la practica 1

---
+++ practica1/ejemplo_hw/hola.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nombre;
+
+    cout << "¿Como te llamas? ";
+    cin >> nombre;
+
+    cout << "Hola, " << nombre << "! Bienvenido a C++." << endl;
+
+    return 0;
+}
