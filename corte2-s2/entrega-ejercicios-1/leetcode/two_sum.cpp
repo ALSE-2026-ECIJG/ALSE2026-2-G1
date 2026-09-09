62c2a61ee9576fbfd6355327111fabf34f0befde
Samuel Lopez <samuel.lopez-c@mail.escuelaing.edu.co>
2026-09-09 14:18:20 -0500

    Optimiza solucion de Two Sum

--- corte2-s2/entrega-ejercicios-1/leetcode/two_sum.cpp
+++ corte2-s2/entrega-ejercicios-1/leetcode/two_sum.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> vistos;
+
     for (int i = 0; i < nums.size(); i++) {
-        for (int j = i + 1; j < nums.size(); j++) {
-            if (nums[i] + nums[j] == target) {
-                return {i, j};
-            }
+        int complemento = target - nums[i];
+
+        if (vistos.find(complemento) != vistos.end()) {
+            return {vistos[complemento], i};
         }
+
+        vistos[nums[i]] = i;
     }
 
     return {};
 }
 
 int main() {
-    vector<int> nums = {3, 2, 4};
-    int target = 6;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
 
     vector<int> resultado = twoSum(nums, target);
 
