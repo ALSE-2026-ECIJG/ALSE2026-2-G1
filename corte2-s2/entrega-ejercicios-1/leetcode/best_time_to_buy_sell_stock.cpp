eac2708d3f7a8124c238c3966d3febdbe52f0071
Samuel Lopez <samuel.lopez-c@mail.escuelaing.edu.co>
2026-09-09 14:50:10 -0500

    Implementa primera solucion de Best Time to Buy and Sell Stock

---
+++ corte2-s2/entrega-ejercicios-1/leetcode/best_time_to_buy_sell_stock.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int maxProfit = 0;
+
+    for (int i = 0; i < prices.size(); i++) {
+        for (int j = i + 1; j < prices.size(); j++) {
+            int ganancia = prices[j] - prices[i];
+
+            if (ganancia > maxProfit) {
+                maxProfit = ganancia;
+            }
+        }
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    int resultado = maxProfit(prices);
+
+    cout << resultado << endl;
+
+    return 0;
+}
